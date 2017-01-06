cf0cdca060f6d2a646a016e37bd86249b9f11ff5
Haruka Takahashi <1124226@st.anan-nct.ac.jp>
2017-01-06 11:56:52 +0900

    delete route

--- dialogue.py
+++ dialogue.py
@@ -1,4 +1,5 @@
 from gensim.models import word2vec
+import GetTweets as gt
 
 global model
 
@@ -41,14 +42,15 @@ def responce(file_name):
   model = word2vec.Word2Vec.load_word2vec_format(file_name, binary=False)
   start = input("You >>")
   goal = "徳島"
-  route = makeRoute(model, start, goal) 
+  #route = makeRoute(model, start, goal) 
   A = start
   #B = goal
-  words = []
-  for r in route[1:]:
-    words.append(r[0])
+  #words = []
+  #for r in route[1:]:
+  #  words.append(r[0])
 
-  for word in words:
+  #for word in words:
+  while True:
     intention_list = []
 
     # Aの類似語を集める　類似度順にソート
@@ -65,17 +67,24 @@ def responce(file_name):
 
     for s in similar_words:
       # 各類似語とゴールとの類似度の比較
-      intention_list.append((s[0],model.similarity(word,s[0])))
+      intention_list.append((s[0],model.similarity(goal,s[0])))
     
     intention_list.sort(key=lambda x: float(x[1]), reverse=True)
     #print(intention_list)
     
-    print("\n単語\t" + word + "との類似度")
+    print("\n単語\t" + goal + "との類似度")
     for il in intention_list:
       print(il[0] + "\t" + str(il[1]))
     
     print("=======================================================")
     print("「" + intention_list[0][0] + "」")
+    tweets = gt.search(A, intention_list[0][0])   ## A,Bを含むTweetの取得
+    print(type(tweets))
+    if len(tweets) == 0:
+      print(A + "といえば、" + intention_list[0][0] + "についてどう思う？")
+    else:
+      twt_text = tweets[0].text
+      print(twt_text)
     A = input()
   
 
---
+++ sample.py
@@ -0,0 +1,3 @@
+import GetTweets as gt
+
+print(gt.search("あああ", "いいい")[0].text)
